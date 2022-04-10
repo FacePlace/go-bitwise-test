f2fcae29eb3431762cb7e98ef3c593a1ac2169fa
ZeroWorldwide <43105109+FacePlace@users.noreply.github.com>
2022-04-10 16:04:52 +0300

    Integer parsers

--- main.go
+++ main.go
@@ -4,7 +4,18 @@ import "fmt"
 
 func main() {
 
-	x := getInt32([]byte{84, 69, 83, 52})
+	var x int8
+	var y int16
+	var z int32
+	var w int64
 
+	x = getInt8([]byte{84})
 	fmt.Println(x)
+	y = getInt16([]byte{84, 69})
+	fmt.Println(y)
+	z = getInt32([]byte{84, 69, 83, 52})
+	fmt.Println(z)
+	w = getInt64([]byte{84, 69, 83, 52, 54, 0, 0, 0})
+	fmt.Println(w)
+
 }
--- utils.go
+++ utils.go
@@ -1,5 +1,35 @@
 package main
 
+func getInt8(buffer []byte) (n int8) {
+	nBytes := 1
+
+	_ = buffer[nBytes-1]
+
+	i := 0
+	for i < nBytes {
+		n |= int8(buffer[i]) << (8 * (i))
+
+		i++
+	}
+
+	return
+}
+
+func getInt16(buffer []byte) (n int16) {
+	nBytes := 2
+
+	_ = buffer[nBytes-1]
+
+	i := 0
+	for i < nBytes {
+		n |= int16(buffer[i]) << (8 * (i))
+
+		i++
+	}
+
+	return
+}
+
 func getInt32(buffer []byte) (n int32) {
 	nBytes := 4
 
@@ -8,6 +38,21 @@ func getInt32(buffer []byte) (n int32) {
 	i := 0
 	for i < nBytes {
 		n |= int32(buffer[i]) << (8 * (i))
+
+		i++
+	}
+
+	return
+}
+
+func getInt64(buffer []byte) (n int64) {
+	nBytes := 8
+
+	_ = buffer[nBytes-1]
+
+	i := 0
+	for i < nBytes {
+		n |= int64(buffer[i]) << (8 * (i))
 
 		i++
 	}
