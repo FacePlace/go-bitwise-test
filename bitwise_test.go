8bb9dde58f914c4748671cfdb3386c25cf6383f0
ZeroWorldwide <43105109+FacePlace@users.noreply.github.com>
2022-04-11 19:01:33 +0300

    Better test structure

--- bitwise_test.go
+++ bitwise_test.go
@@ -6,53 +6,64 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestGetInt(t *testing.T) {
-	sample := []byte{84, 69, 83, 52, 54, 00, 00, 00}
+var sample = []byte{84, 69, 83, 52, 54, 00, 00, 00}
 
-	var test1 int8 = 84
-	var test2 int16 = 17748
-	var test3 int32 = 877872468
-	var test4 int64 = 232806106452
-
-	var test5 uint8 = 84
-	var test6 uint16 = 17748
-	var test7 uint32 = 877872468
-	var test8 uint64 = 232806106452
-
-	actual1 := GetInt8(sample)
-	assert.Equal(t, test1, actual1)
-
-	actual2 := GetInt16(sample)
-	assert.Equal(t, test2, actual2)
-
-	actual3 := GetInt32(sample)
-	assert.Equal(t, test3, actual3)
-
-	actual4 := GetInt64(sample)
-	assert.Equal(t, test4, actual4)
-
-	actual5 := GetUint8(sample)
-	assert.Equal(t, test5, actual5)
-
-	actual6 := GetUint16(sample)
-	assert.Equal(t, test6, actual6)
-
-	actual7 := GetUint32(sample)
-	assert.Equal(t, test7, actual7)
-
-	actual8 := GetUint64(sample)
-	assert.Equal(t, test8, actual8)
+func TestGetUint8(t *testing.T) {
+	var expected uint8 = 84
+	actual := GetUint8(sample)
+	assert.Equal(t, expected, actual)
 }
 
-func TestGetFloat(t *testing.T) {
-	sample := []byte{84, 69, 83, 52, 54, 00, 00, 00}
+func TestGetUint16(t *testing.T) {
+	var expected uint16 = 17748
+	actual := GetUint16(sample)
+	assert.Equal(t, expected, actual)
+}
 
-	var test1 float32 = 1.9676127749335137e-7
-	var test2 float64 = 1.1502149934e-312
+func TestGetUint32(t *testing.T) {
+	var expected uint32 = 877872468
+	actual := GetUint32(sample)
+	assert.Equal(t, expected, actual)
+}
 
-	actual1 := GetFloat32(sample)
-	assert.Equal(t, test1, actual1)
+func TestGetUint64(t *testing.T) {
+	var expected uint64 = 232806106452
+	actual := GetUint64(sample)
+	assert.Equal(t, expected, actual)
+}
 
-	actual2 := GetFloat64(sample)
-	assert.Equal(t, test2, actual2)
+func TestGetInt8(t *testing.T) {
+	var expected int8 = 84
+	actual := GetInt8(sample)
+	assert.Equal(t, expected, actual)
+}
+
+func TestGetInt16(t *testing.T) {
+	var expected int16 = 17748
+	actual := GetInt16(sample)
+	assert.Equal(t, expected, actual)
+}
+
+func TestGetInt32(t *testing.T) {
+	var expected int32 = 877872468
+	actual := GetInt32(sample)
+	assert.Equal(t, expected, actual)
+}
+
+func TestGetInt64(t *testing.T) {
+	var expected int64 = 232806106452
+	actual := GetInt64(sample)
+	assert.Equal(t, expected, actual)
+}
+
+func TestGetFloat32(t *testing.T) {
+	var expected float32 = 1.9676127749335137e-7
+	actual := GetFloat32(sample)
+	assert.Equal(t, expected, actual)
+}
+
+func TestGetFloat64(t *testing.T) {
+	var expected float64 = 1.1502149934e-312
+	actual := GetFloat64(sample)
+	assert.Equal(t, expected, actual)
 }
